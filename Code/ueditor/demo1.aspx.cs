e53896663e1d8861dceed35ccd5436ce933f5810
刘晨辉 <chliu@iwstech.com>
2015-08-07 18:54:53 +0800

    datetime

---
+++ Code/ueditor/demo1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ueditor
+{
+    public partial class demo1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(a.Value))
+            {
+                a.Value = "ff";
+            }
+            var aa = a.Value;
+            var d = Request["editorValue"];
+        }
+    }
+}
