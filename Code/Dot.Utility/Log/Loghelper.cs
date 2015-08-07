3950ffaf72cc33b2860a324871dee5ab81158d48
子弹飞 <liuchenhui00@163.com>
2015-08-07 23:28:48 +0800

    log

---
+++ Code/Dot.Utility/Log/Loghelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Dot.Utility.Log
+{
+    public class Loghelper
+    {
+
+        #region 写文件
+        public static void WriteLog(string content)
+        {
+            try
+            {
+
+                Encoding code = Encoding.GetEncoding("gb2312");
+                var dir = System.AppDomain.CurrentDomain.BaseDirectory + "\\Log\\";
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                string logname = dir + DateTime.Today.Date.ToString("yyyyMMdd") + ".txt";　//保存文件的路径
+                string log = string.Format("\r\n\r\n记录时间:{0}\r\n{1}\r\n---------------------------------------------------------------------------------------------",
+                    DateTime.Now, content);
+                File.AppendAllText(logname, log, code);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        #endregion
+
+        #region 读文件
+        public static string ReadLog(DateTime filename)
+        {
+
+            Encoding code = Encoding.GetEncoding("gb2312");
+            var dir = System.AppDomain.CurrentDomain.BaseDirectory + "\\Log\\";
+            if (Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            string temp = dir + DateTime.Today.Date.ToString("yyyyMMdd") + ".txt";　//保存文件的路径
+            string str = "";
+            if (File.Exists(temp))
+            {
+                StreamReader sr = null;
+                try
+                {
+                    sr = new StreamReader(temp, code);
+                    str = sr.ReadToEnd(); // 读取文件
+                }
+                catch { }
+                sr.Close();
+                sr.Dispose();
+            }
+            else
+            {
+                str = "";
+            }
+
+
+            return str;
+        }
+        #endregion
+    }
+}
---
+++ Code/Dot.UtilityTests/File/FileOperateTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dot.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Dot.Utility.Tests
+{
+    [TestClass()]
+    public class FileOperateTests
+    {
+        [TestMethod()]
+        public void CopyDirTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetPostfixStrTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WriteFileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReadFileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FileAddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FileCoppyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FileDelTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FileMoveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FolderCreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FolderCreateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FileCreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteFolderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CopyDirTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetFoldAllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ListTreeShowTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetFoldAllTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ListTreeShowTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDirectoryLengthTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetFileAttibeTest()
+        {
+           var attibe=  FileOperate.GetFileAttribute("E:\\BaiduYunDownload\\Git-1.8.4-preview20130916.exe");
+        }
+    }
+}
