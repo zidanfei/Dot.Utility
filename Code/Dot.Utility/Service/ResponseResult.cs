276cf94d726f9125eb391d082b5f5550bc712768
子弹飞 <liuchenhui00@163.com>
2015-08-27 14:57:52 +0800

    result

---
+++ Code/Dot.Utility/Service/ResponseResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot.Utility.Service
+{
+    public class ResponseResult
+    {
+        public static Result Response(Func<object> fun)
+        {
+            Result res = new Result();
+            try
+            {
+                res.Data = fun();
+                res.StatusCode = (int)HttpStatusCode.OK;
+                res.Success = true;
+            }
+            catch (Exception ex)
+            {
+                res.Success = false;
+                res.Message = ex.Message;
+                res.StatusCode = (int)HttpStatusCode.InternalServerError;
+                if (Dot.Utility.Log.LogFactory.ExceptionLog.IsErrorEnabled)
+                    Dot.Utility.Log.LogFactory.ExceptionLog.Error(ex);
+            }
+            return res;
+        }
+    }
+}
