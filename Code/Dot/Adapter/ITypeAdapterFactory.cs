5d4031671c8b36ec3ea4c0063c17d08d024f9ff9
子弹飞 <liuchenhui00@163.com>
2015-09-11 18:12:48 +0800

    automapper

---
+++ Code/Dot/Adapter/ITypeAdapterFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dot.Adapter
+{
+    /// <summary>
+    /// Base contract for adapter factory
+    /// </summary>
+    public interface ITypeAdapterFactory
+    {
+        /// <summary>
+        /// 创建类型映射适配器
+        /// </summary>
+        /// <returns>The created ITypeAdapter</returns>
+        ITypeAdapter Create();
+    }
+}
---
+++ Code/Dot/Adapter/TypeAdapterFactory.cs
@@ -0,0 +1,45 @@
+﻿
+using Dot.IOC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dot.Adapter
+{
+    public static class TypeAdapterFactory
+    {
+        #region Members
+
+        private static ITypeAdapterFactory _currentTypeAdapterFactory = null;
+        private static readonly object lock_flag = new object();
+        #endregion
+
+        #region Public Static Methods
+
+        /// <summary>
+        /// Set the current type adapter factory
+        /// </summary>
+        /// <param name="adapterFactory">The adapter factory to set</param>
+        public static void SetCurrent(ITypeAdapterFactory adapterFactory)
+        {
+            _currentTypeAdapterFactory = adapterFactory;
+        }
+        /// <summary>
+        /// Create a new type adapter from currect factory
+        /// </summary>
+        /// <returns>Created type adapter</returns>
+        public static ITypeAdapter CreateAdapter()
+        {
+            if (_currentTypeAdapterFactory == null)
+                lock (lock_flag)
+                {
+                    if (_currentTypeAdapterFactory == null)
+                        _currentTypeAdapterFactory = ObjectContainer.CreateInstance<ITypeAdapterFactory>();
+                }
+
+            return _currentTypeAdapterFactory.Create();
+        }
+        #endregion
+    }
+}
