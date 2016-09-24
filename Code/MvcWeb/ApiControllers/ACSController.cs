39da9aeba78cfee3e65c84f4e59e1217292225c3
子弹飞 <liuchenhui00@163.com>
2016-09-24 10:27:50 +0800

    支持跨域

---
+++ Code/MvcWeb/ApiControllers/ACSController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace MvcWeb.ApiControllers
+{
+    [EnableCors("*", "*", "*")]
+    public class ACSController : ApiController
+    {
+    }
+}
